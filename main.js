5ae41f0cd5acad823987ce21099f2494ec0196ef
Shivakumar M <shivakumarm250@gmail.com>
2022-08-01 19:04:01 +0530

    Completed all sections of portfolio

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var oldScroll = window.pageYOffset;
 var navbar = document.getElementById("nav");
+var form = document.getElementById("mailform");
 
 window.onscroll = function() {
 var currScroll = window.pageYOffset;
@@ -10,3 +11,21 @@ var currScroll = window.pageYOffset;
   }
   oldScroll = currScroll;
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const regex = new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+    const name = form.elements[0].value;
+    const email = form.elements[1].value;
+    const subject = form.elements[2].value;
+    const body = form.elements[3].value;
+
+    if(!name || !email || !subject || !body) {
+        alert("All fields are required");
+    } else if(!regex.test(email)) {
+        alert("Enter correct format of mail");
+    } else {
+        window.open(`mailto:shiva11101998@gmail.com?subject=${subject}&body=${body}\nThanks and Regards,\n${name}`);
+        form.reset();
+    }
+})
